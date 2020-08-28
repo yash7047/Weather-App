ab7a021829571b8eef4b63fbf6a86cd950bd7b65
Ashutosh Kumar <vicky691999@gmail.com>
2020-08-28 21:41:59 +0530

    Front-end part

--- src/index.js
+++ src/index.js
@@ -3,28 +3,32 @@ const express=require('express')
 const ejs=require('ejs')
 const path=require('path')
 const app=express()
+const bodyParser=require('body-parser')
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 //Settuping up port
 const port=process.env.PORT || 3000
 
+
 //Path Configuration
 const view_path=path.join(__dirname,'../template/views')
+const public_path=path.join(__dirname,'../public')
 
 //Setting up ejs and View Locations
 app.set('view engine','ejs')
 app.set('views',view_path);
 
+//setup static directory to serve
+app.use(express.static(public_path))
+
 //<------------ROUTERS------------->
 
 //Homepage
-app.get('',(req,res)=>{
-    res.render('index')
+app.get('/',(req,res)=>{
+    res.render('index',{qs:req.query})
 })
 
-//about section
-app.get('/about',(req,res)=>{
-    res.render('about')
-})
+
 
 //Weather Fetching Section
 app.get('/weather',(req,res)=>{
